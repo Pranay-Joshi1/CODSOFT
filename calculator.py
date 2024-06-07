2746f10522a0986d451b466c016a23094ff7d6f6
Pranay-Joshi1 <pranayjoshinith@gmail.com>
2024-06-07 10:29:06 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,8 +1,10 @@
 def calculator():
     print("Allowed operators: +,-,*,/,**,//,%")
+    #Take inputs from user
     x= float(input("First Number: "))
     choice = input("Operator:  ")
     y= float(input("Second Number: "))
+    #Perform operation based on the choices
     if choice == "+":
         ans = x+y
     elif choice == "-":
